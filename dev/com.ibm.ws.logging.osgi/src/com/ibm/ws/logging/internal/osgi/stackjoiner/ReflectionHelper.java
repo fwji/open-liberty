62b099be4ee09133ddf31ad18b7f9b9dfb03daa1
Kirby Chin <kirby.chin@ibm.com>
2020-08-26 09:45:37 -0400

    Updated borrowed class comment

--- dev/com.ibm.ws.logging.osgi/src/com/ibm/ws/logging/internal/osgi/stackjoiner/ReflectionHelper.java
+++ dev/com.ibm.ws.logging.osgi/src/com/ibm/ws/logging/internal/osgi/stackjoiner/ReflectionHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,7 +20,7 @@ import java.security.PrivilegedAction;
 /**
  * Helper class that performs necessary doPriv during reflection.
  * 
- * This class is borrowed from the monitor-1.0 feature.
+ * This class is borrowed from com.ibm.ws.monitor/src/com/ibm/ws/monitor/internal/ReflectionHelper.java.
  */
 class ReflectionHelper {
 
